836854f6bb60c8eca93f08f0d019fb4620efebc8
Iakona986 <dave.j.hunt@gmail.com>
2026-01-26 15:42:06 -0500

    Added text embeddig as base for semantic search

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
+from semantic_search import *
+from sentence_transformers import SentenceTransformer
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    subparsers.add_parser("verify", help="Verify that the embedding model is loaded")
+    embed_parser = subparsers.add_parser("embed_text", help="Embed a text")
+    embed_parser.add_argument("text", type=str, help="Text to embed")
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
